fb4da9cb0ddabb0eaccf8946cfde42816690ddf5
Sunny Israni <sunny@zoraab.com>
2014-09-12 17:25:10 -0400

    adding devise to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   serialize :desc_words, Array
-  validates_presence_of :email, :username, :personal_rank, :mic_style, :play_style, :desc_words
+  validates_presence_of :email, :personal_rank, :mic_style, :play_style, :desc_words
   validates_uniqueness_of :email, :case_sensitive => false
-  validates_uniqueness_of :username, :case_sensitive => false
 end
