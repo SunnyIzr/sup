744e089acb30e5bd4c4cac35ce1e1bde3e2ccf40
Sunny Israni <sunny@zoraab.com>
2014-09-22 11:21:15 -0400

    creating logic to execute critical factors filtering process

--- app/helpers/match_algo.rb
+++ app/helpers/match_algo.rb
@@ -4,13 +4,16 @@
 
 
 module MatchAlgo
+  MIN_SCHED_OVERLAP = 5
+  MIN_TRAIT_OVERLAP = 1
+  MIN_MATCH_TRAIT_OVERLAP = 1
   ALGO_WEIGHT = {
-    :schedule_overlap = 
-    :age_diff = 
-    :traits = 
-    :match_traits = 
-    :play_style_diff = 
-    :mic_style_diff = 
+    :schedule_overlap = 0.1667
+    :age_diff = 0.1667
+    :traits = 0.1667
+    :match_traits = 0.1667
+    :play_style_diff = 0.1666
+    :mic_style_diff = 0.1666
   }
   extend self
   
@@ -24,6 +27,29 @@ module MatchAlgo
   end
   
   def get_suitable_pop(user)
+    pop = User.where(game_id: user.game.id)
+    pop = cut_prior_matches(user,pop)
+    pop = filter_schedule_overlap_min(user,pop)
+    pop = filter_trait_min(user,pop)
+    pop
+  end
+  
+  def cut_prior_matches(user,pop)
+    prior_matches = user.matched_users
+    pop.select {|match| !prior_matches.include?(match)}
+  end
+  
+  def filter_schedule_overlap_min(user,pop)
+    time_slots = user.time_slots
+    pop.select{|match| (time_slots & match.time_slots).size >= MIN_SCHED_OVERLAP}
+  end
+  
+  def filter_trait_min(user,pop)
+    traits = user.traits
+    match_traits = user.match_traits
+    pop.select do |match|
+      (traits & match.traits).size >= MIN_TRAIT_OVERLAP && (match_traits & match.match_traits).size >= MIN_MATCH_TRAIT_OVERLAP 
+    end
   end
   
   def calc_ranks(user,pop)
@@ -31,7 +57,7 @@ module MatchAlgo
     ranks = {}
     pop.each do |match|
       match_rank = {}
-      match_rank[:schedule_overlap] =
+      match_rank[:schedule_overlap] = 
       match_rank[:age_diff] =
       match_rank[:traits] = 
       match_rank[:match_traits] = 
@@ -42,8 +68,20 @@ module MatchAlgo
     ranks
   end
   
+  def rank_schedule_overlap(user,match,pop)
+    
+  end
+  
   def calc_weighted_avg_ranks(user,ranks)
     # {match: weighted_avg}
-    
+    matches = {}
+    ranks.each do |match,match_ranks|
+      weighted_products = []
+      match_ranks.each do |factor,rank|
+        weighted_products << ALGO_WEIGHT[factor] * rank
+      end
+      matches[match] = weighted_products.sum
+    end
+    matches
   end
 end
