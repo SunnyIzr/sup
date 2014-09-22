dee548e890c03542e88a33b62ae2592e8ed97b8c
Sunny Israni <sunny@zoraab.com>
2014-09-22 12:11:35 -0400

    setting up factories for users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,5 +6,9 @@ FactoryGirl.define do
     mic_style {5}
     play_style {5}
     desc_words ['fun','fast','unstoppable']
+    date_of_birth {Date.new(1980,1,1)}
+    postal_code {'10001'}
+    password {'password'}
+    game {Game.last.nil? ? FactoryGirl.create(:game) : Game.last}
   end
 end
