920d3616ecb616c4da31deb51b3736ec5c4876ba
Sunny Israni <sunny@zoraab.com>
2014-09-22 12:26:40 -0400

    updating games to vary title

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,6 +1,8 @@
+games = %w[Titanfall Battlefield Destiny Minecraft Halo FIFA NBA Plants Watch Duty]
+
 FactoryGirl.define do
   factory :game do
-    title {'Titanfall'}
+    title {games[Game.all.size]}
     platform {Platform.last.nil? ? FactoryGirl.create(:platform) : Platform.last}
   end
 end
