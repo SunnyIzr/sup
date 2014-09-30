e6571114e0f49130e7c35b1da46a79118ab02dbb
Sunny Israni <sunny@zoraab.com>
2014-09-30 17:27:24 -0400

    beginning to set up sign up interface

--- app/controllers/signup_requests_controller.rb
+++ app/controllers/signup_requests_controller.rb
@@ -1,12 +1,11 @@
 class SignupRequestsController < ApplicationController
   def create
     @signup_request = SignupRequest.generate_new(params[:signup_request][:email])
-    redirect_to signup_path(@signup_request)
+    redirect_to signup_path(@signup_request.hex_id)
   end
 
   def show
     @signup_request = SignupRequest.find_by(hex_id: params[:hex_id])
-    render json: @signup_request
   end
   
   private
