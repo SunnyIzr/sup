005acf0d8ff8935cba9afe05cc16cebd01f07a4c
Sunny Israni <sunny@zoraab.com>
2014-10-01 18:13:29 -0400

    adding controller tests to static pages and signup requests

--- spec/controllers/signup_requests_controller_spec.rb
+++ spec/controllers/signup_requests_controller_spec.rb
@@ -3,16 +3,24 @@ require 'rails_helper'
 RSpec.describe SignupRequestsController, :type => :controller do
 
   describe "GET create" do
-    it "returns http success" do
-      post :create
-      expect(response).to have_http_status(:success)
+    it "should create a new signup request with a generated hex key" do
+      post :create, {signup_request: {email: 'john@doe.com'} }
+      expect(SignupRequest.last.email).to eq('john@doe.com')
     end
+    it 'should send initiate a signup request email'
+    it 'should render success json'
   end
 
   describe "GET show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+    it "return a signup page for a specified signup_request hex id" do
+      signup_request = SignupRequest.generate_new('john1@doe.com')
+      get :show, {hex_id: signup_request.hex_id}
+      expect(assigns(:signup_request)).to eq(SignupRequest.last)
+    end
+    it 'should render :show view' do
+      signup_request = SignupRequest.generate_new('john1@doe.com')
+      get :show, {hex_id: signup_request.hex_id}
+      expect(response).to render_template(:show)
     end
   end
 
