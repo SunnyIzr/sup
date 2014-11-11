6eed4baa5dd025f333f6875c0dbf9a166803627f
Sunny Israni <sunny@zoraab.com>
2014-11-11 13:13:30 -0500

    creating game dropdowns for game selection

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -3,6 +3,8 @@ var SignupController = {
     this.activateSliders();
     this.activateTagCheckboxes();
     this.activateUnSlider();
+    this.gameSelect();
+    this.platformSelect();
   },
   activateSliders: function(){
     $('.slider').slider({
@@ -24,14 +26,52 @@ var SignupController = {
       dots: true,               //  Display dot navigation
       fluid: false              //  Support responsive design. May break non-responsive designs
     });
+  },
+  gameSelect: function(){
+    $("select[name='game_title']").change(function(e){
+      SignupModel.getPlatforms()
+    })
+  },
+  platformSelect: function(){
+    $("select[name='platform_id']").change(function(e){
+      SignupModel.getGame();
+    })
+  }
+}
+
+var SignupModel = {
+  getPlatforms: function(){
+    gameTitle = $("select[name='game_title']").val()
+    url = '/games/platforms/' + gameTitle
+    $.getJSON(url,function(platforms){
+      SignupView.updatePlatforms(platforms)
+    }) 
+  },
+  getGame: function(){
+    url = '/games/find_game'
+    gameTitle = $("select[name='game_title']").val()
+    platformId = $("select[name='platform_id']").val()
+    data = {game_title: gameTitle, platform_id: platformId}
+    $.post(url,data,function(res){
+      gameId = res.id
+      SignupView.updateGame(gameId)
+    })
   }
 }
 
 var SignupView = {
   changeHiddenRankInput: function(slider,value){
     $('#' + $(slider).data('target')).val(value)
+  },
+  updatePlatforms: function(platforms){
+    $("select[name='platform_id']").html('')
+    $.each(platforms,function(i,platform){
+      option = "<option value='" + platform.id + "'>" + platform.name + "</option>"
+      $("select[name='platform_id']").append(option)
+    })
+    SignupModel.getGame()
+  },
+  updateGame: function(gameId){
+    $("select[name='user[game_id]']").val(gameId)
   }
-}
-
-var SignupModel = {
 }
