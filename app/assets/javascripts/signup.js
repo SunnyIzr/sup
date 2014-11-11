756d1da277fa2bbea8804134ce934dc3f0d10008
Sunny Israni <sunny@zoraab.com>
2014-11-10 21:25:13 -0500

    adding unslider and fixing up style for sign up process

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -1,7 +1,8 @@
 var SignupController = {
   init: function(){
     this.activateSliders();
-    this.activateTagCheckboxes()
+    this.activateTagCheckboxes();
+    this.activateUnSlider();
   },
   activateSliders: function(){
     $('.slider').slider({
@@ -13,6 +14,16 @@ var SignupController = {
   },
   activateTagCheckboxes: function(){
    $('.tag_checkboxes').buttonset() 
+  },
+  activateUnSlider: function(){
+    $('.banner').unslider({
+      speed: 500,               //  The speed to animate each slide (in milliseconds)
+      delay: 99999999999999999,              //  The delay between slide animations (in milliseconds)
+      complete: function() {},  //  A function that gets called after every slide animation
+      keys: true,               //  Enable keyboard (left, right) arrow shortcuts
+      dots: true,               //  Display dot navigation
+      fluid: false              //  Support responsive design. May break non-responsive designs
+    });
   }
 }
 
