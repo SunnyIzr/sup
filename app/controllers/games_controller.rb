4d05a375a3863421ff54b8426a512e982e6d8ba3
Sunny Israni <sunny@zoraab.com>
2014-11-11 13:13:25 -0500

    small typo

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,7 @@ class GamesController < ApplicationController
     render json: Game.platforms(params[:game_title]).to_json
   end
   def find_game
-    @game = Game.where(title: params[:game_title], platform_id: title[:platform_id]).first
-    render json: @game
+    @game = Game.where(title: params[:game_title], platform_id: params[:platform_id].to_i).first
+    render json: @game.to_json
   end
 end
