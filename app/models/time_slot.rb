10bd8a45a71e30b4e643f940a6b438d2379dafab
Sunny Israni <sunny@zoraab.com>
2014-11-11 21:47:56 -0500

    adding a method to easily obtain time slots given start/end times--with testing

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -6,4 +6,19 @@ class TimeSlot < ActiveRecord::Base
   def weekday_name
     WEEKDAY_NAMES[self.weekday]
   end
+  def self.all_time_slots(schedule)
+    time_slots = []
+    schedule.each do |day,times|
+      time_slots << time_slots(day,times['start_time'],times['end_time'])
+    end
+    time_slots.flatten!.compact!
+  end
+  def self.time_slots(day,start_time,end_time)
+    unless start_time == ''
+      weekday = WEEKDAY_NAMES.index(day.capitalize)
+      TimeSlot.all.select{ |ts| ts.weekday == weekday && ts.hour >= start_time.to_i && ts.hour <= end_time.to_i }
+    else 
+      nil
+    end
+  end
 end
--- spec/models/time_slot_spec.rb
+++ spec/models/time_slot_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'rake'
 
 describe TimeSlot do
   it {should validate_presence_of (:weekday)}
@@ -8,5 +9,28 @@ describe TimeSlot do
   it 'should return day of the week' do
     time_slot = FactoryGirl.create(:time_slot)
     expect(time_slot.weekday_name).to eq('Sunday')
+  end
+  it 'should return all time slots for specific day,start time and end time' do
+    0.upto(6) do |weekday|
+      0.upto(23) do |hour|
+        TimeSlot.create(weekday: weekday, hour: hour)
+      end
+    end
+    day = 'Monday'
+    start_time = 6
+    end_time = 18
+    monday_time_slots = TimeSlot.where(weekday: 1)
+    expect(TimeSlot.time_slots(day,start_time,end_time)).to match_array(monday_time_slots[6..18])
+  end
+  it 'should return all time slots for a full schedule' do
+    0.upto(6) do |weekday|
+      0.upto(23) do |hour|
+        TimeSlot.create(weekday: weekday, hour: hour)
+      end
+    end
+    schedule = {"monday"=>{"start_time"=>"6", "end_time"=>"18"}, "tuesday"=>{"start_time"=>"6", "end_time"=>"18"}, "wednesday"=>{"start_time"=>"", "end_time"=>""}, "thursday"=>{"start_time"=>"", "end_time"=>""}, "friday"=>{"start_time"=>"", "end_time"=>""}, "saturday"=>{"start_time"=>"", "end_time"=>""}, "sunday"=>{"start_time"=>"", "end_time"=>""} }
+    
+    time_slots = TimeSlot.all[30..42] + TimeSlot.all[54..66]
+    expect(TimeSlot.all_time_slots(schedule)).to match_array(time_slots)
   end
 end
