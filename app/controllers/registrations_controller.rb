7b12a998c20fb89719e5f724530a34d6242dd629
Sunny Israni <sunny@zoraab.com>
2014-11-11 21:58:27 -0500

    adding timeslots to new signup and generating json of user upon registartion succesful for demo purposes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,10 +11,10 @@ class RegistrationsController < Devise::RegistrationsController
   end
    
    def create
-    p '*'*500
-    p params
     build_resource(sign_up_params)
     resource.desc_words = resource.desc_words.split(',')
+    time_slots = TimeSlot.all_time_slots(params[:day])
+    resource.time_slots << time_slots
 
     resource_saved = resource.save
     yield resource if block_given?
@@ -22,7 +22,9 @@ class RegistrationsController < Devise::RegistrationsController
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_flashing_format?
         sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
+        # respond_with resource, location: after_sign_up_path_for(resource)
+        hash = {user: User.last, game: User.last.game, platform: User.last.game.platform, time_slots: User.last.time_slots, traits: User.last.traits, match_traits: User.last.match_traits, outside_activities: User.last.outside_activities}
+        render json: hash.to_json
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
         expire_data_after_sign_in!
