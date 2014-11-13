4fd44e68f403dcccfe80fae747f48d78182973b8
Sunny Israni <sunny@zoraab.com>
2014-11-12 22:49:15 -0500

    setting password for all fake users as 'password' and establishing a dependent destroy for all user relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,19 @@ class User < ActiveRecord::Base
   validates_presence_of :username, :personal_rank, :mic_style, :play_style, :desc_words, :date_of_birth, :postal_code, :gamertag, :occupation, :game_id
   validates_uniqueness_of :email, :case_sensitive => false
   validates_uniqueness_of :username, :case_sensitive => false
-  has_many :availabilities
+  has_many :availabilities, dependent: :destroy
   has_many :time_slots, through: :availabilities
-  has_many :player_attributes
+  has_many :player_attributes, dependent: :destroy
   has_many :traits, through: :player_attributes
-  has_many :match_attributes
+  has_many :match_attributes, dependent: :destroy
   has_many :match_traits, through: :match_attributes
-  has_many :preferred_activities
+  has_many :preferred_activities, dependent: :destroy
   has_many :outside_activities, through: :preferred_activities
   belongs_to :game
-  has_many :matches
+  has_many :matches, dependent: :destroy
   has_many :matched_users, through: :matches
-  has_many :requested_friend_requests, class_name: 'FriendRequest', foreign_key: 'requester_id'
-  has_many :incoming_friend_requests, class_name: 'FriendRequest', foreign_key: 'recipient_id' 
-  has_many :friendships
+  has_many :requested_friend_requests, class_name: 'FriendRequest', foreign_key: 'requester_id', dependent: :destroy
+  has_many :incoming_friend_requests, class_name: 'FriendRequest', foreign_key: 'recipient_id', dependent: :destroy
+  has_many :friendships, dependent: :destroy
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id' 
 end
