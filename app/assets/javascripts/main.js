b812f9a82b5615952351d4e4f7d1b992df1624da
Sunny Israni <sunny@zoraab.com>
2014-11-20 23:18:45 -0500

    creating sup notification module

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
   SignupController.init()
   switchLoginSignup();
   closeOffCanvasFromNav();
+  openSupNotifications();
 })
+
+openSupNotifications = function(){
+  $('.open-sups').click(function(e){
+    e.preventDefault();
+    $("#notificationContainer").fadeToggle(300);
+  })
+}
