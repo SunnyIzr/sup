ea725a3f197a342740d03e060388d44c02f4ac61
Sunny Israni <sunny@zoraab.com>
2014-11-20 21:56:34 -0500

    make sure to generate matches if the user does not have any

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,9 @@
 class MatchesController < ApplicationController
   def index
     @user = current_user
+    if @user.matches.empty?
+      @user.generate_new_matches
+    end
     @matches = @user.outstanding_matched_users
   end
 end
