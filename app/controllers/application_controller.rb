6b7ec7fdaa3d063ada69cac3bc0e443645429873
Sunny Israni <sunny@zoraab.com>
2014-11-20 23:37:08 -0500

    configuring devise to take username as login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :password, :password_confirmation,:email, :game_id, :personal_rank, :mic_style, :play_style, :desc_words, :date_of_birth, :gamertag, :occupation, :username, :postal_code, { trait_ids: []}, { match_trait_ids: []}, { outside_activity_ids: []} ) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
   end
 end
