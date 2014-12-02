6d356d61a90e8cfa41270e323f87554bc9e43946
Sunny Israni <sunny@zoraab.com>
2014-12-02 12:48:07 -0500

    user can now compose a new message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,20 +2,28 @@ class MessagesController < ApplicationController
   def index
     @messages = current_user.incoming_messages
     @message = Message.new
+    @user = current_user
   end
   
   def sent
     @messages = current_user.sent_messages
     @message = Message.new
+    @user = current_user
   end
   
   def deleted
     @messages = current_user.deleted_messages
     @message = Message.new
+    @user = current_user
   end
   
   def create
-    p '*'*100
-    p params
+    Message.create(message_params)
+    redirect_to sent_messages_path
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit(:recipient_id, :sender_id, :subject, :body)
   end
 end
