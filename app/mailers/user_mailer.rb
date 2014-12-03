4bbd06b2b177edeba6491e9e11903ae842a7e20f
Sunny Israni <sunny@zoraab.com>
2014-12-03 12:23:24 -0500

    creating a contact form submission method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,8 @@
 class UserMailer < ActionMailer::Base
   default from: 'SqdUp <notifications@sqdup.com>'
+  
+  def new_contact_form_entry(email)
+    subject = "SqdUp: New Contact Form Submimssion"
+    mail(to: email , subject: subject).deliver
+  end
 end
